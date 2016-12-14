b6b3c576fc9d0d1583a519f0acc3695c5cd673b0
DoshiRushabh <radoshi010@gmail.com>
2016-12-14 22:30:52 +0530

    recycler view Data Added 14-12-16

--- app/src/main/java/com/rushabh/importandexport/fragments/MainFragment.java
+++ app/src/main/java/com/rushabh/importandexport/fragments/MainFragment.java
@@ -50,16 +50,22 @@ public class MainFragment extends Fragment {
 
     private void initializeDummyData() {
         topicList = new ArrayList<>();
-        topicList.add(new topic(R.drawable.intro,"This is title1","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."));
-        topicList.add(new topic(R.drawable.intro,"This is title1","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."));
-        topicList.add(new topic(R.drawable.intro,"This is title1","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."));
-        topicList.add(new topic(R.drawable.intro,"This is title1","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."));
-        topicList.add(new topic(R.drawable.intro,"This is title1","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."));
-        topicList.add(new topic(R.drawable.intro,"This is title1","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."));
-        topicList.add(new topic(R.drawable.intro,"This is title1","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."));
-        topicList.add(new topic(R.drawable.intro,"This is title1","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."));
-        topicList.add(new topic(R.drawable.intro,"This is title1","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."));
-        topicList.add(new topic(R.drawable.intro,"This is title1","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."));
+        topicList.add(new topic(R.drawable.introduction,"Introduction","Welcomes you to the exciting world of Import-Export.There are so many opportunities in Import-Export sector.\n" +
+                "This setion give you the basic knowledge about Import-Export & it's very useful to you in this sector."));
+        topicList.add(new topic(R.drawable.internationalbodies,"International Bodies","This section provide information about available international & indian bodies, agencies, organization & board for import-export.\n" +
+                "It also gives you the details about Foreign Trade Policy and Indian Government Laws."));
+        topicList.add(new topic(R.drawable.importexportcycles,"Import Export Cycle","This section provide information about import-export licence & import-export cycle and importance about import-export licence."));
+        topicList.add(new topic(R.drawable.importexportbasics,"Import Export Basics","This section provide information about import-export incoterms, types of ports, types of packing & international payment terms."));
+        topicList.add(new topic(R.drawable.modeoftransport,"Modes of Transport","This section provide information about differnet-different types of transportation, methods of dispatch, containers, freight rates & other modes."));
+        topicList.add(new topic(R.drawable.foreigntrade,"Foregin Trade","This section provide information about GDP growth world map, reason of exports, world's leading importer & exporter, institutions, Trade institutions, various rules & acts, career opporunities & export process."));
+        topicList.add(new topic(R.drawable.incoterms,"Incoterms","This section provide information about International Commercial Terms in details, various definitions, various terms & conditions."));
+        topicList.add(new topic(R.drawable.importdocument,"LC Check List & Export Documentation","This section provide information about L/C Checklist & Export Documentation, various shipment documentation."));
+        topicList.add(new topic(R.drawable.payment,"Payment Terms","This section provide information about various types of payment terms, status holders."));
+        topicList.add(new topic(R.drawable.productandmarcet,"Product And Market Selection","This section provide information about international marketing for various product, product selection & market selection."));
+        topicList.add(new topic(R.drawable.identifyingbuyer,"Identifying Buyers","This section provide information about international marketing for various product, product selection & market selection."));
+        topicList.add(new topic(R.drawable.insuranceandrisk,"Insurance and Risk Management","This section provide information about insurance of product, risk management & types of risk."));
+        topicList.add(new topic(R.drawable.govermentbenifit,"Goverment Benefits","This section provide information about government benefits & incentives for import-export, government various types benefits on exports."));
+        topicList.add(new topic(R.drawable.importdocument,"Import Document","This section provide information about important documentation, registration of importers, identification of supplier, important aspects of import contract & payment formalities."));
         }
 
 }

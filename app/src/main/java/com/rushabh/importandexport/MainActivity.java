c1ae2d9006641e4e9f880a85406183fa8646b69d
DoshiRushabh <radoshi010@gmail.com>
2016-12-15 16:58:32 +0530

    Tool bar animation complete 15-12-16

--- app/src/main/java/com/rushabh/importandexport/MainActivity.java
+++ app/src/main/java/com/rushabh/importandexport/MainActivity.java
@@ -1,15 +1,19 @@
 package com.rushabh.importandexport;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.rushabh.importandexport.fragments.MainFragment;
 
@@ -17,12 +21,41 @@ import com.rushabh.importandexport.fragments.MainFragment;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    TextView title;
+    ImageView shareImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        shareImage = (ImageView) findViewById(R.id.shareImage);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("");
+
+        title = (TextView) findViewById(R.id.title);
+        title.setTextSize(20);
+
+        title.setTranslationY(-(600));
+        title.animate()
+                .translationY(0)
+                .setDuration(500)
+                .setStartDelay(500);
+
+
+        toolbar.setTranslationY(-(500));
+        toolbar.animate()
+                .translationY(0)
+                .setDuration(500)
+                .setStartDelay(300);
+
+        shareImage.setTranslationY(-(700));
+        shareImage.animate()
+                .translationY(0)
+                .setDuration(500)
+                .setStartDelay(700);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -48,29 +81,6 @@ public class MainActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.share) {
-
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")

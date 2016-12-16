b325736e1050233d209b48987a5b7859088a18a2
DoshiRushabh <radoshi010@gmail.com>
2016-12-16 11:50:10 +0530

    Back button complete 16-12-16

--- app/src/main/java/com/rushabh/importandexport/activities/introActivity.java
+++ app/src/main/java/com/rushabh/importandexport/activities/introActivity.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.rushabh.importandexport.R;
 
@@ -20,15 +21,25 @@ public class introActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Introduction");
 
-        final Drawable upArrow = getResources().getDrawable(R.drawable.back);
-        upArrow.setAlpha(100);
+        final Drawable upArrow = getResources().getDrawable(R.drawable.back_arrow);
         upArrow.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
         actionBar.setHomeAsUpIndicator(upArrow);
-        if (actionBar != null) {
+            if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                onBackPressed();
+                return false;
+            default:
+                onBackPressed();
+                return true;
+        }
     }
 
     @Override

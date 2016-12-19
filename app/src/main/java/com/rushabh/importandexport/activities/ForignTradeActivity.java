5681bd593f00d5ba822ae283aec706ca08fd8a4f
DoshiRushabh <radoshi010@gmail.com>
2016-12-19 17:50:40 +0530

    6 activity added complete 19-12-16

---
+++ app/src/main/java/com/rushabh/importandexport/activities/ForignTradeActivity.java
@@ -0,0 +1,54 @@
+package com.rushabh.importandexport.activities;
+
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.rushabh.importandexport.R;
+
+public class ForignTradeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forign_trade);
+        overridePendingTransition(R.anim.trans_left_in, R.anim.trans_left_out);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Introduction");
+
+        final Drawable upArrow = getResources().getDrawable(R.drawable.back_arrow);
+        upArrow.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+        actionBar.setHomeAsUpIndicator(upArrow);
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                onBackPressed();
+                return false;
+            default:
+                onBackPressed();
+                return true;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.trans_right_in, R.anim.trans_right_out);
+    }
+
+
+}

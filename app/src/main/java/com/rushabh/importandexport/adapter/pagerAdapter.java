408b1cb9532d7c1c7c1ba61cb9b0592b2c23e20a
DoshiRushabh <radoshi010@gmail.com>
2017-01-04 17:12:17 +0530

    Special thanks detail added 4-1-2017

--- app/src/main/java/com/rushabh/importandexport/adapter/pagerAdapter.java
+++ app/src/main/java/com/rushabh/importandexport/adapter/pagerAdapter.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 import com.rushabh.importandexport.fragments.HvdDetailsFragment;
+import com.rushabh.importandexport.fragments.SpecialThanksFragment;
 import com.rushabh.importandexport.fragments.companyDetailsFragment;
 
 /**
@@ -25,6 +26,8 @@ public class pagerAdapter extends FragmentPagerAdapter {
                 return new companyDetailsFragment();
             case 1:
                 return new HvdDetailsFragment();
+            case 2:
+                return new SpecialThanksFragment();
             default:
                 return null;
         }
@@ -41,7 +44,9 @@ public class pagerAdapter extends FragmentPagerAdapter {
             case 0:
                 return "App developer";
             case 1:
-                return "Special thanks..";
+                return "Our partner";
+            case 2:
+                return "Special Thanks";
             default:
                 return null;
         }

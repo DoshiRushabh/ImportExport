9d7f85cf4950e48183f88f0888b3b31c21ffb2bc
DoshiRushabh <radoshi010@gmail.com>
2017-03-22 17:26:48 +0530

    minor changes 24-3-2017

--- app/src/main/java/com/rushabh/importandexport/fragments/GovermentBenefitsFragment.java
+++ app/src/main/java/com/rushabh/importandexport/fragments/GovermentBenefitsFragment.java
@@ -3,6 +3,7 @@ package com.rushabh.importandexport.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,7 @@ import com.rushabh.importandexport.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GovermentBenefitsFragment extends Fragment {
+public class GovermentBenefitsFragment extends Fragment implements View.OnClickListener {
 
 
     public GovermentBenefitsFragment() {
@@ -27,9 +28,28 @@ public class GovermentBenefitsFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_goverment_benefits, container, false);
 
-        ((MainActivity) getActivity()).setActionBarTitle("Goverment Benefits");
+        ((MainActivity) getActivity()).setActionBarTitle("Government benifits");
+        v.findViewById(R.id.Link1).setOnClickListener(this);
 
         return v;
     }
 
+    @Override
+    public void onClick(View view) {
+        FragmentTransaction fm = getActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(R.anim.trans_left_in, R.anim.trans_left_out, R.anim.trans_right_in, R.anim.trans_right_out)
+                .addToBackStack(null);
+        WebViewFragment webViewFragment = new WebViewFragment();
+        Bundle bundle = new Bundle();
+        if (view.getId() == R.id.Link1) {
+            bundle.putString("url", "http://dgftcom.nic.in/exim/2000/policy/chap-04.htm");
+            bundle.putString("title", " Advance License ");
+
+        }
+
+        webViewFragment.setArguments(bundle);
+        fm.replace(R.id.MainLayout, webViewFragment)
+                .commit();
+    }
 }
